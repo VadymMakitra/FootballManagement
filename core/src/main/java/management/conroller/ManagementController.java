4a263c1e8ecf57a58a275b165d249b18dbe113ce
Vadym Makitra <hmarax3@gmail.com>
2021-12-30 00:11:18 +0200

    Initial commit

---
+++ core/src/main/java/management/conroller/ManagementController.java
@@ -0,0 +1,10 @@
+package management.conroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/football/management")
+public class ManagementController {
+
+}
