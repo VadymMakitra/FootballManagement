630b4aae828aa9fad2a797fe993df3f00b835e28
VadymMakitra <84317209+VadymMakitra@users.noreply.github.com>
2021-12-31 18:17:42 +0200

    Update AllInfoAboutCommandDtoMapper.java

--- service/src/main/java/management/mapper/AllInfoAboutCommandDtoMapper.java
+++ service/src/main/java/management/mapper/AllInfoAboutCommandDtoMapper.java
@@ -1,13 +1,9 @@
 package management.mapper;
 
 import management.dto.AllInfoAboutCommandDto;
-import management.dto.AllInfoAboutFootballerDto;
 import management.entity.Command;
 import org.modelmapper.AbstractConverter;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class AllInfoAboutCommandDtoMapper extends AbstractConverter<Command, AllInfoAboutCommandDto> {
